e566b44a435b8fae78b9828528a158732ba6929e
idan <idanlips@gmail.com>
2023-12-27 14:42:42 +0200

    fix reverse

--- Reverse.java
+++ Reverse.java
@@ -8,13 +8,13 @@ public class Reverse {
 		//// Put your code here
 		String word = args[0];
 		String reverseWord = "";
-		char middleCharacter = word.charAt(word.length() / 2);
 		int i = word.length() - 1;
 		while (i >= 0) {
 			char c = word.charAt(i);
 			reverseWord = reverseWord + c;
 			i--;
 		}
+		char middleCharacter = reverseWord.charAt((reverseWord.length() / 2));
 		System.out.println(reverseWord);
 		System.out.println("The middle character is " + middleCharacter);
 	}
