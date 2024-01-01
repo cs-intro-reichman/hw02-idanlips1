37be46a6ca500b497ee4317c68aa6844e1cde2a3
idan <idanlips@gmail.com>
2024-01-01 16:37:30 +0200

    fixed one of each

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -44,11 +44,11 @@ public class OneOfEachStats {
 		System.out.println("Number of families with 3 children: " + familiesThree);
 		System.out.println("Number of families with 4 or more children: " + familiesFour);
 		if (max == familiesTwo) {
-			System.out.println("The most common number of children is 2");
+			System.out.println("The most common number of children is 2.");
 		} else if (max == familiesThree) {
-			System.out.println("The most common number of children is 3");
+			System.out.println("The most common number of children is 3.");
 		} else {
-			System.out.println("The most common number of children is 4 or more");
+			System.out.println("The most common number of children is 4 or more.");
 		}
 	}
 
